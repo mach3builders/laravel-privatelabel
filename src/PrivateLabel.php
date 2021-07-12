481dd99e704ffaf716c5995b46734c47800566f5
Jasper Helmich <jasper.helmich@gmail.com>
2021-07-12 17:23:50 +0200

    wip

--- src/PrivateLabel.php
+++ src/PrivateLabel.php
@@ -2,8 +2,23 @@
 
 namespace Mach3builders\PrivateLabel;
 
+use Illuminate\Support\Facades\Cache;
+use Mach3builders\PrivateLabel\Models\PrivateLabel as Model;
+
 class PrivateLabel
 {
+    private $label = null;
+
+    public function getLabel()
+    {
+        if (empty($this->label)) {
+            $this->label = Model::where('domain', request()->server('HTTP_HOST'))
+                                                ->first();
+        }
+
+        return $this->label;
+    }
+
     public function findOwnerById(int $owner_id)
     {
         if ($owner_id === null) {
--- src/Support/Helpers.php
+++ src/Support/Helpers.php
@@ -1,11 +1,13 @@
 <?php
 
+use Mach3builders\PrivateLabel\PrivateLabelFacade;
+
 if (! function_exists('label')) {
     /**
      * Return the current label based on the http host
      */
     function label()
     {
-        return \Mach3builders\PrivateLabel\Models\PrivateLabel::where('domain', request()->server('HTTP_HOST'))->first();
+        return PrivateLabelFacade::getLabel();
     }
 }
