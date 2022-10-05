da92e62c5e259fe279218da0760eda51a7f4fac0
Jasper Helmich <jasper.helmich@gmail.com>
2022-10-05 20:46:08 +0200

    wip

--- src/Jobs/InstallDomain.php
+++ src/Jobs/InstallDomain.php
@@ -26,13 +26,7 @@ class InstallDomain implements ShouldQueue
     {
         $domain_name = substr(strrchr($this->private_label->email, "@"), 1);
 
-        // curl -s --user 'api:key-3084c4562bb3a70642f8dea1128ea02f' \
-        //     -X POST https://api.mailgun.net/v3/domains \
-        //     -F name='watkanschaapwel.nl' \
-        //     -F smtp_password='supersecretpassword'
-
-        // curl -s --user 'api:key-3084c4562bb3a70642f8dea1128ea02f' -X DELETE \
-        //         https://api.mailgun.net/v3/domains/mailinator.net
+     
         $response = Http::asForm()->withBasicAuth('api', config('private-label.mailgun.api_token'))
             ->post('https://api.eu.mailgun.net/v3/domains', [
                 'name' => $domain_name,
