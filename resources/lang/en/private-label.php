88e0a64b20671a2269353856b5a20398319a941a
Peter Kannekens <peterkannekens@gmail.com>
2023-01-20 14:46:28 +0100

    set missing translations

--- resources/lang/en/private-label.php
+++ resources/lang/en/private-label.php
@@ -5,7 +5,6 @@ return [
     'checking_dns_info' => 'Make sure the CNAME record for :domain points to '.config('private-label.domain').' Note: It can take up to 24 hours before a changed DNS setting is visible.',
     'checking_dns' => 'Checking DNS',
     'choose_file' => 'Choose file',
-    'priority' => 'Priority',
     'domain_form_text' => 'Enter here the domain name for your Private Label environment. This domain name must be in your possession. Enter the full name with "www". It can also be a subdomain (subdomain.yourdomain.nl) of your website.',
     'domain' => 'Domain',
     'email_form_text' => 'Enter here the e-mail address that will be shown as the sender address to your customers for system mails, which are sent from your Private Label environment.',
@@ -15,8 +14,10 @@ return [
     If the DNS records have been entered correctly, the settings will be completed and from that moment on, email will be sent with the specified email address.',
     'favicon_form_text' => 'Upload here the icon shown in the browser window tab.',
     'favicon' => 'favicon',
+    'general' => 'General',
     'images' => 'images',
     'info_email' => 'Please add the following dns records to:',
+    'info_email_verified' => 'Domain is verified',
     'logo_app_height' => 'Height logo navbar',
     'logo_dark_form_text' => 'Upload here the logo shown on the login page of the tool. We recommend using a PNG with transparent background.',
     'logo_dark' => 'Dark logo',
@@ -25,19 +26,15 @@ return [
     'logo_login_height' => 'Height logo login screen',
     'name_form_text' => 'Enter here the name that will be shown as sender to your customers with all system mails, which are sent from your Private Label environment.',
     'name' => 'Name',
-    'logo_login_height' => 'Hoogte logo op loginscherm',
     'mail' => 'Email settings',
-    'name_form_text' => 'Enter the domain name for your Private Label environment here. This domain name must be in your possession. Enter the full name with "www". It may also be a subdomain (subdomain.yourdomainname.nl) of your website. ',
-    'name' => 'Name',
-    'info_email_verified' => 'Domein is verified',
-    'value' => 'Value',
+    'priority' => 'Priority',
     'private-label' => 'Private label',
-    'private label' => 'Private label',
     'save' => 'Save',
-    'saved_and_installing_email' => 'Settings saved',
-
+    'value' => 'Value',
     'verify' => 'Verify domain',
     'your_platform' => 'Your platform',
+    'saved_and_installing_email' => 'Settings saved',
+
     'info' => '<strong> Attention! </strong> <br>
         We recommend that you first create the CNAME record in the DNS of your domain name. <br>
         Create a CNAME record for the chosen (sub) domain with the value: <strong>'.config('private-label.domain').'</strong>. <br>
