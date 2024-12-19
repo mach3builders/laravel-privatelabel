e74f66f214b9b806a0a6e792968942b03d6a37ee
jasper <jasper.helmich@gmail.com>
2024-12-19 15:34:14 +0100

    wip

--- src/PrivateLabelServiceProvider.php
+++ src/PrivateLabelServiceProvider.php
@@ -3,15 +3,20 @@
 namespace Mach3builders\PrivateLabel;
 
 use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\Http;
+use Spatie\LaravelPackageTools\Package;
 use Mach3builders\PrivateLabel\Console\Reinstall;
 use Mach3builders\PrivateLabel\Console\UpdateLabelPhp;
-use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
 
 class PrivateLabelServiceProvider extends PackageServiceProvider
 {
     public function bootingPackage()
     {
+        if (app()->environment('testing')) {
+            Http::preventStrayRequests();
+        }
+
         return Gate::define('viewPrivateLabel', function ($user, $owner_id) {
             return app()->environment(['local', 'testing']);
         });
--- tests/Feature/InstallSiteTest.php
+++ tests/Feature/InstallSiteTest.php
@@ -2,32 +2,100 @@
 
 namespace Mach3builders\PrivateLabel\Tests\Unit;
 
-use Mach3builders\PrivateLabel\Jobs\InstallSite;
-use Mach3builders\PrivateLabel\Models\PrivateLabel;
-use Mach3builders\PrivateLabel\Services\Forge;
-use Mach3builders\PrivateLabel\Tests\BaseTestCase;
-use Mach3builders\PrivateLabel\Tests\Fixtures\Owner;
 use Mockery\MockInterface;
+use Illuminate\Support\Facades\Http;
+use Mach3builders\PrivateLabel\Services\Forge;
+use Mach3builders\PrivateLabel\Jobs\InstallSite;
+use Mach3builders\PrivateLabel\Tests\BaseTestCase;
+use Mach3builders\PrivateLabel\Models\PrivateLabel;
+use Mach3builders\PrivateLabel\Tests\Fixtures\Owner;
 
 class InstallSiteTest extends BaseTestCase
 {
     /** @test */
     public function will_start_installing_site()
     {
-        $this->mock(Forge::class, function (MockInterface $mock) {
-            $mock->shouldReceive('createSite')->andReturn((object) [
-                'id' => 1,
-            ])->once()
-                ->shouldReceive('updateNginx')->once()
-                ->shouldReceive('createCertificate')->once();
-        });
+        Http::fake([
+            'localhost*' => Http::response([], 200),
+            'https://mach3builders.nl/' => Http::response([], 200),
+        ]);
 
         $owner = Owner::factory()->create();
         $private_label = PrivateLabel::factory()->create([
+            'domain' => 'mach3builders.nl',
             'owner_id' => $owner->id,
             'status' => 'dns_validated',
         ]);
 
-        InstallSite::dispatch($private_label);
+        (new InstallSite(($private_label)))->handle();
+    }
+
+    /** @test */
+    public function will_retry_if_dns_is_not_validated()
+    {
+        Http::fake([
+            'localhost*' => Http::response([], 200),
+            'https://mach3builders.nl/' => Http::response([], 200),
+        ]);
+
+        $owner = Owner::factory()->create();
+        $private_label = PrivateLabel::factory()->create([
+            'domain' => 'mach3builders.nl',
+            'owner_id' => $owner->id,
+            'status' => 'dns_pending',
+        ]);
+
+        (new InstallSite(($private_label)))->handle();
+
+        $this->assertDatabaseHas('private_labels', [
+            'id' => $private_label->id,
+            'status' => 'dns_pending',
+        ]);
+    }
+
+    /** @test */
+    public function will_update_status_to_site_installing()
+    {
+        Http::fake([
+            'localhost*' => Http::response([], 200),
+            'https://mach3builders.nl/' => Http::response([], 404),
+        ]);
+
+        $owner = Owner::factory()->create();
+        $private_label = PrivateLabel::factory()->create([
+            'domain' => 'mach3builders.nl',
+            'owner_id' => $owner->id,
+            'status' => 'dns_validated',
+        ]);
+
+        (new InstallSite(($private_label)))->handle();
+
+        $this->assertDatabaseHas('private_labels', [
+            'id' => $private_label->id,
+            'status' => 'site_installing',
+        ]);
+    }
+
+    /** @test */
+    public function will_update_status_to_site_installed()
+    {
+        Http::fake([
+            'localhost*' => Http::response([], 200),
+            'https://mach3builders.nl/' => Http::response([], 200),
+        ]);
+
+        $owner = Owner::factory()->create();
+        $private_label = PrivateLabel::factory()->create([
+            'domain' => 'mach3builders.nl',
+            'owner_id' => $owner->id,
+            'status' => 'dns_validated',
+        ]);
+
+        (new InstallSite(($private_label)))->handle();
+
+        $this->assertDatabaseHas('private_labels', [
+            'id' => $private_label->id,
+            'status' => 'site_installed',
+        ]);
     }
 }
